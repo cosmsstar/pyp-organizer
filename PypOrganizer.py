eda95d0a3aa0223b2007e2b0d2c87e4738a26b71
Yeh Yu Cheng <eltonyeh1030@gmail.com>
2022-05-17 17:40:38 +0800

    Create the `ProblemOrganizer` class

--- PypOrganizer.py
+++ PypOrganizer.py
@@ -1,11 +1,11 @@
-class ProblemExtractor:
+class ContentExtractor:
     """
     A class that stores the content of a file as a list of strings.
     """
 
     def __init__(self, path: str):
         """
-        Constructs a ProblemExtractor according to the given file path.
+        Constructs a ContentExtractor according to the given file path.
         :param path: The path of the file.
         """
         self.path = path
@@ -13,53 +13,64 @@ class ProblemExtractor:
             self.lines = file.readlines()
 
     def __str__(self):
-        return self.path
+        return f"ContentExtractor @ {self.path}"
 
     def print_content(self):
-        print_content(self.lines)
+        for line in self.lines:
+            print(line, end='')
 
 
-def filter_content(extractor: ProblemExtractor):
+class ProblemOrganizer:
     """
-    Filter out contents that are not part of the answers / questions, including the titles.
-    :param extractor: the ProblemExtractor we will deal with.
-    :return: the filtered list of content.
+    A class that parses a tex file to problems.
     """
-    return list(filter(lambda line: line.startswith('    ') or not line.strip(),
-                       extractor.lines))
 
+    def __init__(self, extractor: ContentExtractor = None, path: str = None):
+        if not extractor and not path:
+            raise Exception("Please provide a path or a ProblemExtractor!")
 
-def print_content(contents):
-    for line in contents:
-        print(line, end='')
-
-
-def group_problems(extractor: ProblemExtractor):
-    group = []
-    contents = filter_content(extractor)
-    is_start = False
-    current_problem = ''
-    for line in contents:
-        is_start = is_start or line.strip().startswith('%->')
-        if not is_start:
-            continue
-        if line.strip().startswith('%->') and current_problem:
-            group.append(current_problem.rstrip())
-            current_problem = line
+        if not extractor:
+            self.extractor = ContentExtractor(path)
         else:
-            current_problem += line
+            self.extractor = extractor
 
-    if current_problem:
-        group.append(current_problem.rstrip())
+    def __str__(self):
+        return self.extractor.__str__()
 
-    return group
+    def __filter_content(self):
+        """
+        Filter out contents that are not part of the answers / questions, including the titles.
+        :param extractor: the ContentExtractor we will deal with.
+        :return: the filtered list of content.
+        """
+        return list(filter(lambda line: line.startswith('    ') or not line.strip(), self.extractor.lines))
 
+    def group_problems(self):
+        group = []
+        contents = self.__filter_content()
+        is_start = False
+        current_problem = ''
+        for line in contents:
+            is_start = is_start or line.strip().startswith('%->')
+            if not is_start:
+                continue
+            if line.strip().startswith('%->') and current_problem:
+                group.append(current_problem.rstrip())
+                current_problem = line
+            else:
+                current_problem += line
 
-def extract_keys(problem: str):
-    """
-    Extract the problem number as a key.
-    :param problem: The problem to be processed.
-    :return: the number of the problem.
-    """
-    numbers = problem.split()[1].split('-')
-    return tuple(map(lambda num: int(num), numbers))
+        if current_problem:
+            group.append(current_problem.rstrip())
+
+        return group
+
+    @staticmethod
+    def extract_keys(problem: str):
+        """
+        Extract the problem number as a key.
+        :param problem: The problem to be processed.
+        :return: the number of the problem.
+        """
+        numbers = problem.split()[1].split('-')
+        return tuple(map(lambda num: float(num), numbers)), problem
