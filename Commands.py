b38f2b10cc005567723097175e572deb266424d5
Yeh Yu Cheng <eltonyeh1030@gmail.com>
2022-05-19 18:01:38 +0800

    Add config file function

--- Commands.py
+++ Commands.py
@@ -1,6 +1,6 @@
 import os
 from PypOrganizer import ProblemOrganizer
-from functools import cmp_to_key
+import ConfigOrganizer
 
 
 def __find_all_occurrence(base: str, name: str):
@@ -27,24 +27,38 @@ def group_problems(base: str, name: str):
 
 
 def sort_problems(problems):
-    def __compare_problem_keys(problem1, problem2):
-        key1, key2 = problem1[0], problem2[0]
-        if key1[0] != key2[0]:
-            return key1[0] - key2[0]
-        return key1[1] - key2[1]
-
-    problems_with_keys = map(ProblemOrganizer.extract_keys, problems)
-    return sorted(problems_with_keys, key=cmp_to_key(__compare_problem_keys))
+    problems_with_keys = sorted(map(ProblemOrganizer.extract_keys, problems), key=lambda p: p[0][1])
+    sections = set(n for n in map(lambda p : p[0][0], problems_with_keys))
+    result = dict()
+    for s in sections:
+        result[s] = [p[1] for p in problems_with_keys if p[0][0] == s]
+    return result
 
 
-def organize_problems(base: str, name: str, dest: str, dest_name: str):
+def generate_problems(base: str, name: str, config_path: str):
     problems = sort_problems(group_problems(base, name))
+    output = [ConfigOrganizer.generate_preamble(config_path),
+              ConfigOrganizer.BEGIN_DOCUMENT,
+              ConfigOrganizer.generate_title(config_path)]
+    for section in sorted(problems.keys()):
+        section_problem = problems[section]
+        output.append(ConfigOrganizer.BEGIN_QUESTION)
+        for problem in section_problem:
+            output.append(problem + '\n\n')
+        output.append(ConfigOrganizer.END_QUESTION)
+    output.append(ConfigOrganizer.END_DOCUMENT)
+
+    return output
+
+
+def organize_problems(base: str, name: str, dest: str, dest_name: str, config_path: str):
+    problems = generate_problems(base, name, config_path)
     if not os.path.exists(dest):
         os.makedirs(dest)
     dest_file = os.path.join(dest, dest_name)
     with open(dest_file, 'w', encoding='utf-8-sig') as file:
-        file.writelines(map(lambda pro: f'{pro[1]}\n\n', problems))
+        file.writelines(problems)
 
 
 if __name__ == '__main__':
-    organize_problems('Test', '104.tex', 'out/Test', '104.tex')
+    organize_problems('Test', '104.tex', 'out/Test', '104.tex', 'Test/config/104.json')
--- ConfigOrganizer.py
+++ ConfigOrganizer.py
@@ -1,6 +1,12 @@
 import json
 
 
+BEGIN_DOCUMENT = '\\begin{document}\n\n'
+END_DOCUMENT = '\\end{document}\n'
+BEGIN_QUESTION = '\\begin{cquestions}\n\n'
+END_QUESTION = '\\end{cquestions}\n\n'
+
+
 def generate_preamble(file: str):
     with open(file, 'r', encoding='utf-8-sig') as file:
         data = json.load(file)
@@ -14,16 +20,18 @@ def generate_preamble(file: str):
 def generate_title(file: str):
     with open(file, 'r', encoding='utf-8-sig') as file:
         data = json.load(file)
-        return f'\\helloworld{{{data["year"]}}}{{{data["school"]}}}{{{data["class"]}}}{{{data["subject"]}}}\n'
+        return f'\\helloworld{{{data["year"]}}}{{{data["school"]}}}{{{data["class"]}}}{{{data["subject"]}}}\n\n'
 
 
-def generate_section(file: str, index: int, content: str):
+def generate_section(file: str, index: int):
     with open(file, 'r', encoding='utf-8-sig') as file:
         data = json.load(file)
         title = data['sections'][index - 1]
-        section_head = f'\\section{{{title}}}\n'
-        return section_head + '\\begin{cquestions}\n' + content + '\n\\end{cquestions}\n'
+        explanation = data['section_explanations'][index - 1]
+        if not explanation:
+            return f'\\section{{{title}}}\n\n'
+        return f'\\section{{{title}}}\n\n{explanation}\n\n'
 
 
 if __name__ == '__main__':
-    print(generate_section("Test/config/104.json", 1, '\tabcabc'))
+    print(generate_section("Test/config/104.json", 1))
